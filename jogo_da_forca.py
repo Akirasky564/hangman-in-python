151076ab407bd8fe7bac69185c95fd50a0ab23c6
Akirasky564 <progcarol.estudos@gmail.com>
2026-07-15 21:20:17 -0300

    Processa o palpite do jogador

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,5 +1,13 @@
 lista_palavras = ["python", "programacao", "desenvolvimento", "jogo", "forca"]
 
+def inicializar_jogo():
 palavra = random.choice(lista_palavras)
 tentativas_set = set() 
 return palavra, tentativas_set
+
+def processar_tentativa(letra, palavra_secreta, letras_acertadas):
+    if letra in palavra_secreta:
+        letras_acertadas.add(letra)
+        return True
+    else:
+        return False
