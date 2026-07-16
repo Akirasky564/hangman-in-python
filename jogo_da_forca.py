f0f0a64841344ddb07f3ed8d65a76278d9f964b5
Akirasky564 <progcarol.estudos@gmail.com>
2026-07-15 21:25:56 -0300

    adiciona a dinamica da palavra oculta e os status para o jogador

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -12,7 +12,7 @@ def processar_tentativa(letra, palavra_secreta, letras_acertadas):
     else:
         return False
 
-'''titulo do jogo'''
+#titulo do jogo
 print("Jogo da Forca\n") 
 
 palavra_secreta, letras_acertadas = inicializar_jogo()
@@ -20,3 +20,16 @@ chances = 6
 
 while True:
     palavra_oculta = ""
+
+for letra in palavra_secreta:
+        if letra in letras_acertadas:
+            palavra_oculta += letra + " "  # Mostra a letra se já foi acertada
+        else:
+            palavra_oculta += "_ "    
+        
+print(f"\nPalavra atual: {palavra_oculta}")
+    print(f"Chances restantes: {chances}")        
+
+  if "_" not in palavra_oculta:
+        print("Parabéns! Você descobriu a palavra por completo!")
+        break     
